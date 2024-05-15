b4dc80043753e3b582738305723ef44001c66225
kobinaq <kobinastudios@gmail.com>
2024-05-15 11:41:23 +0000

    Task 7 completed

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,14 @@
+#usr/bin/python3
+
+#Define prototpye
+def islower(c):
+
+    #convert alphabeth to Ascii code
+    asciicode = ord(c)
+
+    #conditinal statements to check if ASCII code falls within the lowercase number range
+
+    if 92 < asciicode < 123:
+        return True
+    else:
+        return False
