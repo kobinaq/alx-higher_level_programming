589926900a59d59d39725def87895a2e50d33f41
kobinaq <kobinastudios@gmail.com>
2024-05-15 12:46:53 +0000

    Task 8 updated

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -9,7 +9,8 @@ def uppercase(str):
     # Convert only lowercase letters to uppercase
     for i in range(len(chrList)):
         asciiCode = ord(chrList[i])
-        if 96 < i < 123:
+
+        if 96 < asciiCode < 123:
             chrList[i] = chr(asciicode - 32)
 
     uppercaseStr = "".join(chrList)
