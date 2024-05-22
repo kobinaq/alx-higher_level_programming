01a1d8c53bf912df0466a833c9680a5159c453d3
kobinaq <kobinastudios@gmail.com>
2024-05-22 05:35:37 +0000

    Introduction to sql Task 1 - 7 upload

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,13 @@
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA 
+FROM 
+    information_schema.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'hbtn_0c_0' 
+    AND TABLE_NAME = 'first_table';
+
