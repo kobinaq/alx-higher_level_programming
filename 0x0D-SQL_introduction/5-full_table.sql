d4bec9fddb8dc8c8927347fb17a387ef7fd3b561
kobinaq <kobinastudios@gmail.com>
2024-05-22 05:51:10 +0000

    Task 9 - 16 completed

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,13 +1,12 @@
-SELECT 
-    COLUMN_NAME, 
-    COLUMN_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_KEY, 
-    COLUMN_DEFAULT, 
-    EXTRA 
-FROM 
-    information_schema.COLUMNS 
-WHERE 
-    TABLE_SCHEMA = 'hbtn_0c_0' 
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM
+    information_schema.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'hbtn_0c_0'
     AND TABLE_NAME = 'first_table';
-
