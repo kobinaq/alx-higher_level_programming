63f5f3cd4a9dbc17d94680a3f9bbe3fbcf416ec4
kobinaq <kobinastudios@gmail.com>
2024-07-13 15:29:17 +0000

    Task 11 updated

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -3,7 +3,7 @@
 import sys
 
 # print text to standard error using write command in sys
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-1")
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19")
 
 # Exit with status code 1
 sys.exit(1)
