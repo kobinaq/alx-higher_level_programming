04650fa6880aa7526b58a351241ceaa3bbdc4924
kobinaq <kobinastudios@gmail.com>
2024-07-13 16:00:37 +0000

    Task 11 updates

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,16 +2,4 @@
 # Multiply two numbers
 
 def pow(a, b):
-
-        if (b < 0):
-            a = 1 / a
-            b = -b
-
-        elif (b == 0):
-            return 1
-
-        result = 1
-        for _ in range(b):
-            result *= a 
-
-        return result
+    return a**b
